0abc99b8aed1c339d145a581ed474844266ae8eb
Ayush pal <palayu2001@gmail.com>
2026-02-19 22:16:41 +0530

    used vite production level url

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "VITE_API_URL/api/v1/course";
+const COURSE_API = `${import.meta.env.VITE_API_URL}/api/v1/course`;
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
